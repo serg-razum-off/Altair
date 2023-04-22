00519194cacf933f917e8a3c59c819f310dd02e7
SR <sergiy.razumov@gmail.com>
2023-04-22 14:15:51 +0300

    SR [2023-04-22]: Flights from and to Airports
    >> validation of arrived airplanes vs departed airplanes
    >> function to delete vars updated
    >> Flights/ Destination by Years -- started

--- Functions/General_Func.py
+++ Functions/General_Func.py
@@ -22,11 +22,12 @@ def delete_variables(variables_list):
         # Check if the variable exists in locals()
         if variable_name in frame_locals:
             exec(f'del {variable_name}', frame_globals, frame_locals)
-            print(f">>> Variable '{variable_name}' deleted from locals().")
+            print(f">>> Variable '{variable_name}'{'.'* (30 - len(variable_name))} deleted from locals().")
         # Check if the variable exists in globals()
         elif variable_name in frame_globals:
             exec(f'del {variable_name}', frame_globals, frame_globals)
-            print(f">>> Variable '{variable_name}' deleted from globals().")
+            print(f">>> Variable '{variable_name}'{'.'* (30 - len(variable_name))} deleted from globals().")
         else:
             print(f">>> Variable '{variable_name}' does not exist.")
     gc.collect
+
