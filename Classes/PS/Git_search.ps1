6ea3fd5ab83e3ecc728c7406aacca7786afa2980
SR <sergiy.razumov@gmail.com>
2023-05-15 10:23:36 +0300

    SR [2023-05-15]: Git Search Function update
    >> git: gitserchResults moved to a separate .ps1 --> much speed boost gained

---
+++ Classes/PS/Git_search.ps1
@@ -0,0 +1,59 @@
+# Define the SearchResult class
+class SearchResult {
+    [datetime]$Date
+    [string]$BranchName
+    [string]$CommitHash
+    [string]$FileName
+    [int]$LineNumber
+    [string]$LineContent
+}
+# Define the Search-GitCommits function
+function Search-GitCommits {
+    <#
+    $regexPattern="#.SR.*"
+    $branchName = "kggl_videogames"
+
+    $results = Search-GitCommits -BranchName $branchName -RegexPattern $regexPattern
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$BranchName,
+        [Parameter(Mandatory = $true)]
+        [string]$RegexPattern
+    )
+
+    # Step 2: grab all Commits of the Branch
+    $commits = git log --format="%h" $BranchName
+
+    # Step 3: loop through the text of Commits and search for the RegEx pattern
+    $results = New-Object System.Collections.ArrayList
+    foreach ($commit in $commits) {
+        $date = git show --format="%ci" -s $commit
+        $files = git show --pretty="" --name-only $commit
+        foreach ($file in $files) {
+            $lines = git show "$commit`:$file"
+            
+                $lineNumber = 0
+                foreach ($line in $lines) {
+                    $lineNumber++
+                    if ($line -match $regexPattern) {
+                        # Step 4: create a SearchResult object and add it to the array
+                        $searchResult = [SearchResult]@{
+                            Date = [datetime]::ParseExact($date.Trim(), "yyyy-MM-dd HH:mm:ss zzz", $null)
+                            BranchName = $branchName
+                            CommitHash = $commit
+                            FileName = $file
+                            LineNumber = $lineNumber
+                            LineContent = $line
+                        }
+                        [void]$results.Add($searchResult)
+                    }
+                }
+                
+        }  
+    }
+
+    # Step 5: return the results
+    return $results
+}
